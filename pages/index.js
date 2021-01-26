82ee9410594cab3e14b4cd298b4ef0878dc4bbfb
devMarilia <devMarilia.frontend@gmail.com>
2021-01-26 08:36:37 -0300

    Primeiro component react com next

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+function Title(props){
+  return(
+    <h1>{props.children}</h1>
+  )
+}
 export default function Home() {
   return <Title>My page</Title>
 }
